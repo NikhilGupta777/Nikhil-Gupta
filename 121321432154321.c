c71171cd2e067586c95094b42dbbaa143e0163c4
Nikhil gupta <2022a1r090@mietjammu.in>
2023-01-13 00:10:50 +0530

    Add files via upload

---
+++ 121321432154321.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int i,n,j;
+    printf("enter value:");
+    scanf("%d",&n);
+    for(i=i;i<=n;i++){
+        for(j=i;j<=i;++j)
+        printf("%d",j);
+        printf("\n");
+
+    }
+}
